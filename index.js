7398b7dc8086ed3526cae514d6d28fb89741da23
Gignesh k j <kjgignesh@gmail.com>
2023-12-20 08:42:48 +0530

    jsonserver

---
+++ index.js
@@ -0,0 +1,19 @@
+
+// import json-server package
+const jsonServer =require('json-server');
+//create json server
+const mediaPlayerServer=jsonServer.create();
+// create a connection to db.json file
+const router= jsonServer.router('db.json');
+//create a middleware : inermediate function inorder to use all this created object
+const middleware= jsonServer.defaults();
+// use this middleware
+mediaPlayerServer.use(middleware);
+mediaPlayerServer.use(router);
+
+
+const port=5000;
+mediaPlayerServer.listen(port,()=>{
+    console.log(`json server is up and running is port ${port}`);
+})
+
